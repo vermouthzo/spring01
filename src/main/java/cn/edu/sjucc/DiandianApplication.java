1424247550adcd2025d31f865dea1408bdc38464
lemon <64025550+zgShelby@users.noreply.github.com>
2020-05-21 13:21:26 +0800

    修改注册拦截器的方法，避免使用淘汰方法。

--- src/main/java/cn/edu/sjucc/DiandianApplication.java
+++ src/main/java/cn/edu/sjucc/DiandianApplication.java
@@ -1,5 +1,6 @@
 package cn.edu.sjucc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
@@ -8,13 +9,23 @@ import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
-public class DiandianApplication {
-
+public class DiandianApplication implements WebMvcConfigurer{
+	
+	@Autowired
+	AuthInterceptor authInterceptor;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DiandianApplication.class, args);
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor);
 	}
 	
 	@Bean
